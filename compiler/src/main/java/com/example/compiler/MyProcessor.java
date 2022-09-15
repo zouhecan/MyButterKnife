c632a699b162c443baef3b75c6f5e255c2925dc2
zouhecan <hc.zou@ctrip.com>
2022-09-15 21:53:40 +0800

    first commit

--- compiler/src/main/java/com/example/compiler/MyProcessor.java
+++ compiler/src/main/java/com/example/compiler/MyProcessor.java
@@ -116,7 +116,7 @@ public class MyProcessor extends AbstractProcessor {
                 classBuilder.addMethod(MethodSpec.methodBuilder("getAuthor")
                         .addModifiers(Modifier.PUBLIC)
                         .returns(String.class)
-                        .addStatement("return $N", "\"<hc.zou>\"")
+                        .addStatement("return $N", "\"<zouhecan>\"")
                         .build()
                 );
 
